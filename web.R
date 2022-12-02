5311a4d8649e7db47ace9e139ed52f79d5b7b4c0
Vega Aitor <aitorv17@gmail.com>
2022-12-02 11:40:46 +0100

    commit web

--- web.R
+++ web.R
@@ -1,4 +1,4 @@
 install.packages("blogdown") # install the blogdown package
 library(blogdown) # load blogdown
-new_site(theme = "aitor-vega/starter-academic") # create your website!
+new_site(theme = "wowchemy/starter-academic") # create your website!
 
